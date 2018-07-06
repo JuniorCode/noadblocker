077156461bc02b5afe5e11fe9625610a562f34f7
JuniorCode <JuniorCode@users.noreply.github.com>
2018-07-06 15:08:41 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,1 @@
+var xhttp=new XMLHttpRequest;xhttp.open("get","adsbygoogle.js",!0),xhttp.send(),xhttp.onreadystatechange=function(){0===this.status&&!0===navigator.onLine&&(location.href="/adblocker")};
